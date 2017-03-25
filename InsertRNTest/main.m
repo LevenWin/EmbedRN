815c13670649fa88aee0e99474bd161f35b65cc1
leven <leven@levendembp.lan>
2017-03-25 12:29:25 +0800

    Initial Commit

---
+++ InsertRNTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InsertRNTest
+//
+//  Created by leven on 2017/3/25.
+//  Copyright © 2017年 leven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
